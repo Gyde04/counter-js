b034a5e25eb9215f9e0dd3dbb4416bab2542862b
Gyde04 <fakileemmanuel@gmail.com>
2024-02-14 06:20:04 +0100

    add reset state

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-const decreaseBtn = document.getElementById()
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
