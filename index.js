7f8852fbe465bbbdfd11de62b2b2ffd3b13ba9da
Gyde04 <fakileemmanuel@gmail.com>
2024-02-17 06:20:04 +0100

    feat: count function

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 const countLabel = document.getElementById("countLabel")
+
+let count = 0;
